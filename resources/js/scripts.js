eefaa520f1ebd82de6259e5d9838a362d74ca77a
Christopher A Phelan <phelca2@farmingdale.edu>
2018-01-30 11:56:46 -0500

    Added media queries in queries.css to make page responsive. Also added jQuery

---
+++ resources/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    
+    // Turn h1 elements red when h1 is clicked
+    $('h1').click(function(){
+        $(this).css('background-color','#f40808');
+    })
+    
+});
